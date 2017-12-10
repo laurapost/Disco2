dfab4a6127fa07ba57b7506d4be621fd13a4a1f5
Laura Post <laurapost@lauras-macbook-pro.local>
2017-12-10 13:58:17 -0600

    Initial Commit

---
+++ Disco2(singleView)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Disco2(singleView)
+//
+//  Created by Laura Post on 12/10/17.
+//  Copyright © 2017 Laura Post. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
